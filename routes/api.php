6b3ffb680a21582aeab9980ec1c11e92b7381b8d
zulfauzi92 <64009520+zulfauzi92@users.noreply.github.com>
2021-02-25 20:25:10 +0700

    revisi API

--- routes/api.php
+++ routes/api.php
@@ -9,24 +9,24 @@ use App\Http\Controllers\SubscribersController;
 
 //Group Controller
 Route::group(['prefix' => 'group'], function() {
-    Route::get('/read', [GaleryController::class, 'index']);
-    Route::post('/create', [GaleryController::class, 'store']);
-    Route::post('/update/{id}', [GaleryController::class, 'update']);
-    Route::delete('/delete/{id}', [GaleryController::class, 'destroy']);
+    Route::get('/read', [GroupController::class, 'index']);
+    Route::post('/create', [GroupController::class, 'store']);
+    Route::post('/update/{id}', [GroupController::class, 'update']);
+    Route::delete('/delete/{id}', [GroupController::class, 'destroy']);
 });
 
 //GroupSubscribers Controller
 Route::group(['prefix' => 'group-subscribers'], function() {
-    Route::get('/read', [GaleryController::class, 'index']);
-    Route::post('/create', [GaleryController::class, 'store']);
-    Route::post('/update/{id}', [GaleryController::class, 'update']);
-    Route::delete('/delete/{id}', [GaleryController::class, 'destroy']);
+    Route::get('/read', [GroupSubscribersController::class, 'index']);
+    Route::post('/create', [GroupSubscribersController::class, 'store']);
+    Route::post('/update/{id}', [GroupSubscribersController::class, 'update']);
+    Route::delete('/delete/{id}', [GroupSubscribersController::class, 'destroy']);
 });
 
 //Subscribers Controller
 Route::group(['prefix' => 'subscribers'], function() {
-    Route::get('/read', [GaleryController::class, 'index']);
-    Route::post('/create', [GaleryController::class, 'store']);
-    Route::post('/update/{id}', [GaleryController::class, 'update']);
-    Route::delete('/delete/{id}', [GaleryController::class, 'destroy']);
+    Route::get('/read', [SubscribersController::class, 'index']);
+    Route::post('/create', [SubscribersController::class, 'store']);
+    Route::post('/update/{id}', [SubscribersController::class, 'update']);
+    Route::delete('/delete/{id}', [SubscribersController::class, 'destroy']);
 });
